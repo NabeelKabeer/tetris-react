d26437f1898c45803bc2cab727253c9c2ba0329a
nabeel.kabeer <nabeel.kabeer@servicenow.com>
2019-12-02 15:00:42 +0530

    Updates

---
+++ precache-manifest.bcd37b4c3ae11dd005f849cdbe539681.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "5ffb780af0f8d98f40e3",
+    "url": "/tetris-react/static/js/runtime~main.009b4986.js"
+  },
+  {
+    "revision": "fd320e586c046fd95268",
+    "url": "/tetris-react/static/js/main.91b86fc9.chunk.js"
+  },
+  {
+    "revision": "681b51057d74cfa8975d",
+    "url": "/tetris-react/static/js/2.65aa1cca.chunk.js"
+  },
+  {
+    "revision": "fd320e586c046fd95268",
+    "url": "/tetris-react/static/css/main.fad260fe.chunk.css"
+  },
+  {
+    "revision": "14ddd0a9a4988fe84127e519d987f41b",
+    "url": "/tetris-react/index.html"
+  }
+];
